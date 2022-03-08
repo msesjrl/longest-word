f86f06546f3486b77b857a30df5a40e9754624da
Mannnny <38955706+msesjrl@users.noreply.github.com>
2022-03-08 19:12:11 +0000

    Create main.py

---
+++ Main/main.py
@@ -0,0 +1,34 @@
+with open("allwords.txt") as f:
+    lines = f.readlines()
+
+banned = input("Enter all banned letters separated by a space (example: a b c): ")
+banned = banned.split()
+same = []
+current = 0
+longest = ""
+
+for line in lines:
+    current += 1
+    line = line.lower()
+    word_banned = False
+    for letter in line:
+        if letter in banned:
+            word_banned = True
+    if not word_banned:
+        if len(line) > len(longest):
+            longest = line
+            print("INFO >>> New longest:", longest, end="")
+            same = []
+        if len(line) == len(longest):
+            same.append(line)
+    if current % 10000 == 0:
+        print("INFO >>> Searched", current, "words")
+
+for i in range(3):
+    print("\n")
+
+print("Words of the same length without banned letters:\n")
+for i in range(len(same)):
+    print(str(i+1) + ".", same[i], end="")
+
+selfEnd = input("\nPress Enter to Continue...")
